532df77f4134e60d754f15ec70d5f17fcd6497b8
guohengcai <5711@ait.nsw.edu.au>
2018-05-04 16:49:38 +1000

    Add css

--- ts/main-module.ts
+++ ts/main-module.ts
@@ -1,117 +1,48 @@
-class Task{
-  id: string;
-  name: string;
-  status: boolean;
-  constructor(taskname: string){
-    this.id = new Date().getTime().toString();
-    this.name = taskname;
-    this.status = false;
-  }
-}
+// import modules from other files
+import * as moment from 'moment';
+import { ListView } from '../ts/listview';
+import { Task } from '../ts/task';
+import { Template } from '../ts/template';
+import { TaskManager } from '../ts/taskmanager';
+import { DataStorage } from '../ts/datastorage';
 
-class DataStorage{
-  constructor(){
-
-  }
-  retrieve(){
-
-  }
-  store( tasks:Array <Task> ){
-
-  }
-}
-
-class TaskManager{
-  tasks: Array<Task>;
-  constructor(arrayname: Array<Task>){
-    this.tasks = arrayname;
-  }
-  add(task:Task){
-    this.tasks.push(task);
-  }
-  remove(id:string){
-    let index_to_remove:number = undefined;
-    this.tasks.forEach( (item:Task, index:number) => {
-      if(item.id  == id){
-        index_to_remove = index;
-      }
-    });
-    if( index_to_remove !== undefined){
-      this.tasks.splice( index_to_remove, 1 );
+function getParentId(elm:Node){
+  while( elm.parentNode ){
+    elm = elm.parentNode;
+    let id:string = (<HTMLElement> elm).getAttribute('id');
+    if( id ){
+      return id;
     }
   }
-  changeStatus(id:string,callback):void{
-    this.tasks.forEach( (task:Task) => {
-      if(task.id  === id){
-        if( task.status == false ){
-          task.status = true;
-          return;
-        }
-        else{
-          task.status = false;
-        }
-      }
-    });
-    callback();
-    console.log( this.tasks );
-  }
-
+  return null;
 }
-
-class Template{
-  template:string;
-  constructor(){
-    //not being used right now
-  }
-  populate(id:string, name:string, status:string){
-    let task:string =  `<li id="${id}" data-status="${status}">
-                <div class="task-container">
-                <div class="task-name">${name}</div>
-                <div class="task-buttons">
-                  <button type="button" data-function="done">&#x2714;</button>
-                  <button type="button" data-function="delete">&times;</button>
-                </div>
-                </div>
-            </li>`;
-    return task;
-  }
-}
-
-class ListView{
-  list: HTMLElement;
-  constructor( listid: string ){
-    this.list = document.getElementById('task-list');
-  }
-  clear(){
-    this.list.innerHTML = '';
-  }
-  render( items:Array<Task> ){
-    //clear the view
-    //render array using template
-    items.forEach( (task) => {
-    let id= task.id;
-    let name = task.name;
-    let status = task.status.toString();
-    let item = tasktemplate.populate(id,name,status);
-    // convert our string to HTML Node
-    let fragment = document.createRange().createContextualFragment(item);
-    this.list.appendChild( fragment );
-    });
-  }
-}
-
 //----INITIALISE CLASSES
 //array to store tasks
 var taskarray: Array<Task> = [];
+//storage class
+var taskstorage = new DataStorage('taskdata');
 //Task Manager class, pass the task array
 var taskmanager = new TaskManager( taskarray );
 //list view
 var listview = new ListView('task-list');
 //task template
-var tasktemplate = new Template();
+export var tasktemplate = new Template();
 
 
-
+//things to do when app loads
+window.addEventListener('load',init);
+function init(){
+  //read tasks from storage and display
+  taskstorage.read( (data) => {
+    if( data.length > 0 ){
+      data.forEach( (item) => {
+        taskarray.push( item );
+      });
+      listview.clear();
+      listview.render( taskarray );
+    }
+  });
+}
 //reference to form
 const taskform:HTMLFormElement = (<HTMLFormElement>document.getElementById('task-form'));
 //add listener to form
@@ -119,25 +50,49 @@ taskform.addEventListener('submit', ( event: Event) => {
   event.preventDefault();
   let input:HTMLElement = document.getElementById('task-input');
   let taskname: string = (<HTMLInputElement>input).value;
-  let task:Task = new Task(taskname);
-  taskmanager.add( task);
-  taskform.reset();
-  listview.clear();
-  listview.render( taskarray );
+  //prevent blank tasks form being created
+  if( taskname.length > 0 ){
+    let task:Task = new Task(taskname);
+    taskmanager.add( task);
+    taskstorage.store( taskarray, ( result ) => {
+      if( result ){
+        taskform.reset();
+        listview.clear();
+        listview.render( taskarray );
+      }
+      else{
+        //show error message / call error handler
+      }
+    });
+  }
 });
 
+//--LIST STUFF
 //add listener to list
 const listelement:HTMLElement = document.getElementById('task-list');
 //add listener to list
 listelement.addEventListener('click', (event:Event) => {
-  console.log(event.target);
   let target:HTMLElement = <HTMLElement> event.target;
-  if( target.getAttribute('data-function') == 'done' ){
-    let id = target.getAttribute('id');
-    taskmanager.changeStatus(id,() => {
-      listview.clear();
-      listview.render( taskarray );
-    });
-  }
+  let id = getParentId( (<Node> event.target) );
 
+  if( target.getAttribute('data-function') == 'status' ){
+    if( id ){
+      taskmanager.changeStatus(id,() => {
+        taskstorage.store( taskarray , () => {
+          listview.clear();
+          listview.render( taskarray );
+        });
+      });
+    }
+  }
+  if( target.getAttribute('data-function') == 'delete'){
+    if( id ){
+      taskmanager.remove( id, () => {
+        taskstorage.store( taskarray, () => {
+          listview.clear();
+          listview.render( taskarray );
+        });
+      });
+    }
+  }
 });
